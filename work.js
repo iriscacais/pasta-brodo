749aa18c24f9fc76d69d1ecc335a7c082362705f
Iris Cacais <112999212+iriscacais@users.noreply.github.com>
2025-07-07 12:31:02 -0300

    link de redirect

--- work.js
+++ work.js
@@ -9,7 +9,7 @@ fetch("/content/projetos/projetos.json")
     .reverse()
       .map(
         (projeto) => `
-        <a href="/projeto.html?slug=${projeto.slug}" class="project-card">
+        <a href="/projetos/${projeto.slug}" class="project-card">
           <div class="card-thumb" style="background-image: url('${projeto.thumb}')">
             <div class="card-overlay">
               <h2 class="card-title">${projeto.titulo}</h2>
